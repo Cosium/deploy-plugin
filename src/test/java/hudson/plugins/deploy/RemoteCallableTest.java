37b8ed22a17f15054d450a130e423fc43254d452
Julien Béti <jbeti@cosium.com>
2019-09-12 20:20:29 +0200

    JENKINS-51440 Configure alternative context for each Tomcat container

--- src/test/java/hudson/plugins/deploy/RemoteCallableTest.java
+++ src/test/java/hudson/plugins/deploy/RemoteCallableTest.java
@@ -74,7 +74,7 @@ public class RemoteCallableTest {
                 new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL, "test-id", "", "user", "pass"));
 
         ArrayList<ContainerAdapter> adapters = new ArrayList<ContainerAdapter>();
-        adapters.add(new Tomcat8xAdapter("http://example.com", "test-id", "/manager/text", StringUtils.EMPTY, "test-id"));
+        adapters.add(new Tomcat8xAdapter("http://example.com", "test-id", StringUtils.EMPTY, "/manager/text"));
         project.getPublishersList().add(new DeployPublisher(adapters, war.getName()));
 
         Run<?, ?> run = project.scheduleBuild2(0).get();
